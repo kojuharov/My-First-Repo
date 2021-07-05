92272fc1a7009ecdac155c562c399b14f5f114e9
Georgi Kojuharov <georgikojuharov@Georgi-Kojuharov-2.local>
2021-07-05 10:52:35 +0300

    adding while_loop.php

---
+++ while_loop.php
@@ -0,0 +1,20 @@
+<?php
+$i = 1;
+while ( $i < 100 ) {
+$i++;
+if ( $i % 3 === 0 && $i % 5 === 0 ) {
+echo "Chisloto e $i - ";
+echo 'FooBar';
+echo "\n";
+}
+if ( $i % 3 === 0 ) {
+echo "Chisloto e $i - ";
+echo 'Foo';
+echo "\n";
+}
+if ( $i % 5 === 0 ) {
+echo "Chisloto e $i - ";
+echo 'Bar';
+echo "\n";
+}
+}
